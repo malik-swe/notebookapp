dcb0dcc7aabb5b3e24750e050a6c3929685e2e0b
Abdul Málik Abubacar <abdulabubacar.swe@gmail.com>
2026-01-15 14:38:17 +0100

    first commit: Initial Spring Boot project setup

---
+++ src/main/java/com/example/notebookapp/NotebookappApplication.java
@@ -0,0 +1,13 @@
+package com.example.notebookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotebookappApplication.class, args);
+    }
+
+}
